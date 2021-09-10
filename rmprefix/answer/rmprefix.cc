24ccad8864fe3b3a99ce0b446dd92155def627f4
John Ordoyo <gtom_1984@hotmail.ca>
2021-09-10 11:44:26 -0700

    HW0: create simple solution

---
+++ rmprefix/answer/rmprefix.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+const char * WHITE_SPACE = "\t ";
+
+int main() {
+    for (std::string line; std::getline(std::cin, line);) {
+        std::cout << line.erase(0, line.find_first_not_of(WHITE_SPACE)) << std::endl;
+    }
+	exit(EXIT_SUCCESS);
+}
